490a2e2fb45f00a2182c2450b2fddc47cc27fe9b
andresrivera <andresrivera.web@gmail.com>
2023-02-16 20:01:08 -0800

    task 6 output

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of a char:%2d byte(s)\n", sizeof(char));
 printf("Size on an int:%2d byte(s)\n", sizeof(int));
 printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
 printf("Size of a lon long int:%2d byte(s)\n", sizeof(long int *));
-printf("Size of a float:%2d byte(s)\n", sizeof(float));
+printf("Size of a float:%2d byte(s) \n", sizeof(float));
 return (0);
 }
