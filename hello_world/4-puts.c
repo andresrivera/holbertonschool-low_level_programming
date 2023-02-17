86785e9dc294af50ea041e8f45de51981f74fde3
andresrivera <andresrivera.web@gmail.com>
2023-02-16 18:50:05 -0800

    fix git push

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - function
+* description - task 4: print text using the function puts
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
--- hello_world/README.md
+++ hello_world/README.md
@@ -1,1 +1,3 @@
 C - Hello, World
+
+0-
