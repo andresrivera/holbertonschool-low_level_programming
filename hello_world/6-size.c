48359b432da979592c28ed3587db422bc90f7b56
andresrivera <andresrivera.web@gmail.com>
2023-02-16 19:53:32 -0800

    task 6 push

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function
+* description - prints the size of various types
+*
+* Return: Alaways 0
+*/
+int main(void)
+{
+printf("Size of a char: %2d byte(s)\n", sizeof(char);
+printf("Size on an int: %2d byte(s)\n", sizeof(int);
+printf("Size of a long int: %2d byte(s)\n", sizeof(long int);
+printf("Size of a lon long int: %2d byte(s)\n", sizeof(long int *);
+printf("Size of a float: %2d byte(s)\n", sizeof(float);
+return (0);
+}
