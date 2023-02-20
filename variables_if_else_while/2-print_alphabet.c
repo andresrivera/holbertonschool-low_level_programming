6d4695722b520524b350bdd63979bc99ee35a96d
andresrivera <andresrivera.web@gmail.com>
2023-02-20 13:56:22 -0800

    task 2:I sometimes suffer from insomnia

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - funtio
+* description - Task 2:  I sometimes suffer from insomnia
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= 'z'; c++)
+{
+putchar(c);
+printf("\n");
+}
+
+return (0);
+}
