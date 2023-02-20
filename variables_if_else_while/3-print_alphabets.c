19bc1d09f12f4cddf1655fd32a95ad457bdef3db
andresrivera <andresrivera.web@gmail.com>
2023-02-20 14:30:10 -0800

    fix task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function
+* description - Task 3: alphABET
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
