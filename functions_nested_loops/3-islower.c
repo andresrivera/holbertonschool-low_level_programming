5546cb74f2abdb50e8fa106555d67fa88278015d
andresrivera <andresrivera.web@gmail.com>
2023-02-21 14:08:07 -0800

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _islower(int c) - function
+* description - Check for lowercase characters
+*
+* Return: 0
+*/
+int _islower(int c)
+{
+if (islower(c) != 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar('1');
+}
+return (0);
+}
