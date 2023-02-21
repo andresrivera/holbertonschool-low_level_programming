c2b56043d64c2ed7fa8f93be82b12fde468e2f7c
andresrivera <andresrivera.web@gmail.com>
2023-02-21 11:15:43 -0800

    task 1: print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet
+* Description: Print all the alphabet
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+for (char c = 'a'; c <= 'z', c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
