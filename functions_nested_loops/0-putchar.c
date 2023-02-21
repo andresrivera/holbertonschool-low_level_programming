fc78575627c007201629d44c713ffd44c63e7cb3
andresrivera <andresrivera.web@gmail.com>
2023-02-21 10:26:17 -0800

    task 0: _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - function
+* _putchar - writes the character c to stdout  @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
