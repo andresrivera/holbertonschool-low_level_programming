93bfe70c8240664933283ffcbd16bbff0f3cede6
andresrivera <andresrivera.web@gmail.com>
2023-02-20 18:40:50 -0800

    Task 5:Numberz

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - function
+* description - Task 5: Print numbers from 0 to 10
+*
+* Return: Always 0
+*/
+int main(void)
+{int number;
+for (number = 0; number <= 9; number++)
+{
+printf("%d", number);
+}
+putchar('\n');
+
+return (0);
+}
