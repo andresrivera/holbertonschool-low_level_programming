25c0e6db41c036f9319b5bd967cab1118542c539
andresrivera <andresrivera.web@gmail.com>
2023-02-22 04:49:05 -0800

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,7 +11,9 @@ void jack_bauer(void)
 int hour_a, hour_b, min_a, min_b;
 for (hour_a = 0; hour_a <= 2; hour_a++)
 {
-for (hour_b = 0; hour_b < 24; hour_b++)
+for (hour_b = 0; hour_b <= 9; hour_b++)
+{
+if ((hour_a <= 1 && hour_b <= 9) || (hour_a <= 2 && hour_b <= 3))
 {
 for (min_a = 0; min_a < 6; min_a++)
 {
@@ -23,6 +25,7 @@ _putchar(':');
 _putchar(min_a + '0');
 _putchar(min_b + '0');
 _putchar('\n');
+}
 }
 }
 }
