9a130b0125d91a58f8f197abc3657c57c06e3d3b
andresrivera <andresrivera.web@gmail.com>
2023-02-24 04:16:31 -0800

    Task 6: Shortest distance

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - function
+* @n: number that gives the times it prints
+* Description: prints n times the character '_'
+*
+* Return: Always 0
+*/
+void print_line(int n)
+{
+for (; n >= 0; n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
