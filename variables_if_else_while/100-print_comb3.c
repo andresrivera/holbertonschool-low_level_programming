f36c87c2a13670cb2d2ee66e1969e07b91a8124c
andresrivera <andresrivera.web@gmail.com>
2023-02-24 10:07:32 -0800

    Task 11: Success combination

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* description - print all combination of 2 digits to 100
+*
+*Return: Always 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (i < j)
+{
+putchar(i + '0');
+putchar(j + '0');
+if(i != 9 && j != 9)
+{
+putchar(' ');
+}
+}
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
