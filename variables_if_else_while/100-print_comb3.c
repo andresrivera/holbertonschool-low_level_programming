9ff7e4f3d3da7291accaf2f5562791741e729bf6
andresrivera <andresrivera.web@gmail.com>
2023-02-24 15:44:50 -0800

    Task 10: Fix

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-if (i < j && i != j)
+if (i < j && i != j && i < 9)
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
