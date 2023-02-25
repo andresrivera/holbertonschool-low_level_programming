d7934118f558001ff4fecadf54075027096f1fa9
andresrivera <andresrivera.web@gmail.com>
2023-02-24 20:56:54 -0800

    fix task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,27 +7,26 @@
 * Return: Always 0;
 */
 int main(void)
-{
 int count;
-long f1 = 0, f2 = 1, sum;
-long f1_half1, f1_half2, f2_half1, f2_half2;
+long fibonacci1 = 0, fibonacci2 = 1, sumatory;
+long fibonacci1_half1, fibonacci1_half2, fibonacci2_half1, fibonacci2_half2;
 long half1, half2;
 for (count = 0; count < 92; count++)
 {
-sum = f1 + f2;
-printf("%lu, ", sum);
-f1 = f2;
-f2 = sum;
+sumatory = fibonacci1 + fibonacci2;
+printf("%lu, ", sumatory);
+fibonacci1 = fibonacci2;
+fibonacci2 = sumatory;
 }
-f1_half1 = f1 / 10000000000;
-f2_half1 = f2 / 10000000000;
-f1_half2 = f1 % 10000000000;
-f2_half2 = f2 % 10000000000;
+fib1_half1 = fibonacci1 / 10000000000;
+fib2_half1 = fibonacci2 / 10000000000;
+fib1_half2 = fibonacci1 % 10000000000;
+fib2_half2 = fibonacci2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
-half1 = f1_half1 + f2_half1;
-half2 = f1_half2 + f2_half2;
-if (f1_half2 + f2_half2 > 9999999999)
+half1 = fibonacci1_half1 + fibonacci2_half1;
+half2 = fibonacci1_half2 + fibonacci2_half2;
+if (fibonacci1_half2 + fibonacci2_half2 > 9999999999)
 {
 half1 += 1;
 half2 %= 10000000000;
@@ -35,10 +34,10 @@ half2 %= 10000000000;
 printf("%lu%lu", half1, half2);
 if (count != 98)
 printf(", ");
-f1_half1 = f2_half1;
-f1_half2 = f2_half2;
-f2_half1 = half1;
-f2_half2 = half2;
+fibonacci1_half1 = fibonacci2_half1;
+fibonacci1_half2 = fibonacci2_half2;
+fibonacci2_half1 = half1;
+fibonacci2_half2 = half2;
 }
 printf("\n");
 return (0);
