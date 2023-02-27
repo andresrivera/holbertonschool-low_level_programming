7e877cc27690e0077762e8c5c04461d99c03a5a1
andresrivera <andresrivera.web@gmail.com>
2023-02-27 07:33:32 -0800

    Fix Task 0 betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,12 @@
 
 /**
 * reset_to_98 - function
-* @n integer
+* @n: pointer to integer
 * Description: takes a ponter to an int as parameter and updates value to 98
 *
 * Return: Always 0
 */
 void reset_to_98(int *n)
 {
-*n = 98; 
+*n = 98;
 }
