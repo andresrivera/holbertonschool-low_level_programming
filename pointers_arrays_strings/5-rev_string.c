60fddf8e4c86c5738d6e0924feb0dc4719fb1d2b
andresrivera <andresrivera.web@gmail.com>
2023-02-28 17:53:51 -0800

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+* rev_string - function
+* @s: pointer type char s
+* Description: Function that prints a string in reversee
+* Return: Always 0 
+*/
+void rev_string(char *s)
+{
+int n = 0;
+while (*s)
+{
+n++;
+_putchar(*s++);
+}
+_putchar('\n');
+for (; n > 0; n--)
+{
+_putchar(*s--);
+}
+_putchar('\n');
+}
+
