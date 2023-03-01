3e91201c99db171a7a90dcceb85df29943e6ae02
andresrivera <andresrivera.web@gmail.com>
2023-03-01 12:58:35 -0800

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_array - function
+* @a: pointer to array a
+* @n: number of elements of the array to be printed
+* Description: prints n elements of an array
+*
+* Return: Always 0;
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+if (a[i] != '\0')
+{
+for (; i <= n; i++)
+{
+printf("%d", a[i]);
+if (i != n)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+}
