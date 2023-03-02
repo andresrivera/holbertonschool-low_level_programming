b4cb623a0c19f7949d2cc0000ea4b5a931931536
andresrivera <andresrivera.web@gmail.com>
2023-03-02 13:52:34 -0800

    Task 0

---
+++ pointers_arrays_strings_01/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _atoi - function.
+* @s: pointer to char string
+*
+* Return: Always 0.
+*/
+int _atoi(char *s)
+{
+int sign = 1;
+unsigned int number = 0;
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+number = (number * 10) + (*s - '0');
+else if (number > 0)
+break;
+} while (*s++);
+return (number *sign);
+}
---
+++ pointers_arrays_strings_01/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - function
+* @s: pointer type char s
+* Description: Function that prints a string in reverse
+*
+* Return: Always 0
+*/
+void print_rev(char *s)
+{
+int n = 0;
+while (s[n] != 0)
+{
+n++;
+}
+for (n = n - 1; n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
+}
