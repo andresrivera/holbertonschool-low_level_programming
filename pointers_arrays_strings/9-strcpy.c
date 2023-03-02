91e91c75ed8dcc6a6aab679271c59e2f7ddbccf5
andresrivera <andresrivera.web@gmail.com>
2023-03-02 03:50:09 -0800

    Task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
-while (src[i])
+char *i = dest;
+for (; *src != '\0'; src++)
 {
-dest[i] = src[i];
-i++;
+*dest = *src;
+dest++;
 }
-*dest = '\0';
-return (dest);
+*dest = *src;
+return (i);
 }
