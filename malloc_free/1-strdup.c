4a0ce07b9deea20f1278ccca940fa034b7c7dc04
andresrivera <andresrivera.web@gmail.com>
2023-03-14 19:47:24 -0700

    Task

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *_strdup - function
+* @str: string to duplicate
+* Description: return pointer to a duplicate string
+*/
+char *_strdup(char *str)
+{
+char *cpstr;
+unsigned int i;
+if (str == 0)
+{
+return (0);
+}
+for (i = 0; str[i]; i++)
+;
+cpstr = malloc(sizeof(char) * (i + 1));
+if (cpstr == 0)
+{
+return (0);
+}
+cpstr[i] = '\0';
+while (i--)
+{
+cpstr[i] = str[i];
+}
+return (cpstr);
+}
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *str_concat - function
+* @s1: content
+* @s2: content
+* Description: pointer to new content in memory space
+*/
+char *str_concat(char *s1, char *s2)
+{
+int a, b, c;
+char *str;
+a = b = 0;
+if (s1 != NULL)
+for (a = 0; s1[a]; a++)
+;
+if (s2 != NULL)
+for (b = 0; s2[b]; b++)
+;
+str = malloc(sizeof(char) * (a + b + 1));
+if (str == NULL)
+return (NULL);
+c = 0;
+while (c < (a + b))
+{
+if (c < a)
+str[c] = s1[c];
+else
+str[c] = s2[c - a];
+c++;
+}
+str[c] = 0;
+return (str);
+}
