ad3e00e78e358eb22fe9276ccee179d20cf504d4
andresrivera <andresrivera.web@gmail.com>
2023-03-17 21:20:17 -0700

    Task1

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /**
- * malloc_checked - fuction that allocates memory and also checks if suceful
-* @b: size of memory to allocate.
-*
-* Return: pointer to memory, Else if return exit status 98.
+* *malloc_checked - allocate memory with malloc
+* @b: unsigned int type
+* Return: return pointer
 */
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
+int *ptr;
 ptr = malloc(b);
 if (ptr == NULL)
-{
-free(ptr);
 exit(98);
-}
 return (ptr);
 }
