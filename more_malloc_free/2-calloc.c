21d989ae5e9256377a42b0deb4b30ecb3c03c8b4
andresrivera <andresrivera.web@gmail.com>
2023-03-17 21:12:13 -0700

    Task1

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,36 +1,29 @@
-#include "holberton.h"
+#include "notrebloh.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* _bzero - set all the bytes to 0.
-* @dst: pointer to propigate.
-* @size: of the memory per-byte.
-*
-* Return: pointer to memory area dst.
-*/
-void *_bzero(void *dst, unsigned int size)
-{
-char *d = dst;
-do
-* d++ = 0;
-while (--size != 0);
-return (d);
-}
-/**
-* _calloc - create and array using calloc
-* @nmemb: number elements.
-* @size: size of the type.
-*
-* Return: pointer to the memory.
+* *_calloc - function to allocates memory
+* @nmemb: unsigned int type
+* @size: unsigned int type
+* Return: return pointer to array
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
-if (!nmemb || !size)
+char *ptr;
+unsigned int count;
+if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = malloc(nmemb * size);
-if (!ptr)
+if (ptr == NULL)
+{
 return (NULL);
-_bzero(ptr, nmemb * size);
+}
+count = 0;
+while (count < nmemb * size)
+{
+ptr[count] = 0;
+count++;
+}
 return (ptr);
 }
