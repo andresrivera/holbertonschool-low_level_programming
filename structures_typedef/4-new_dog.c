e6899c36ec94a59effd884ee94fdf08e36b9781f
andresrivera <andresrivera.web@gmail.com>
2023-03-23 19:57:01 -0700

    Task 4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,36 +1,69 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
-* new_dog - entry point
-* @name: string from main, name of pet
-* @age: number from main, age of pet
-* @owner: string from main, owner of pet
-* Return: p
+* _strdup - create a new array containing a copy of the given string
+* @str: a pointer to the string to copy
+*
+* Return: NULL if str is NULL or if memory allocation fails,
+* otherwise a return a pointer to the new copy
 */
 
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int size = 0;
+if (str)
+{
+while (str[size++])
+;      
+dup = malloc(sizeof(char) * size);
+if (dup)
+{
+while (size--)
+dup[size] = str[size];
+return (dup);
+}
+}
+return (NULL);
+}
+
+/**
+* new_dog - create a new dog
+* @name: the new dog's name
+* @age: the new dog's age
+* @owner: the new dog's owner
+*
+* Return: a pointer to the new dog, or NULL if memory allocation fails
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *p;
-/* reserving memory to struct*/
-p = malloc(sizeof(dog_t));
-if (p == NULL)
+dog_t *d;
+d = malloc(sizeof(dog_t));
+if (!d)
 return (NULL);
-/* Cpunting name pointer*/
-if (name == NULL)
+if (name)
 {
-free(p);
-free(owner);
+d->name = _strdup(name);
+if (!(d->name))
+{
+free(d);
 return (NULL);
 }
-if (owner == NULL)
+}
+else
+d->name = NULL;
+d->age = age;
+if (owner)
 {
-free(p);
-free(name);
+d->owner = _strdup(owner);
+if (!(d->owner))
+{	  
+free(d->name);
+free(d);
 return (NULL);
 }
-p->name = name;
-p->age = age;
-p->owner = owner;
-return (p);
+}
+else
+d->owner = NULL;
+return (d);
 }
