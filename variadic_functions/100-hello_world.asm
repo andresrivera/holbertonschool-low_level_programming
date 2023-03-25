af1b11861189abce995f863ded2cdf064ea54c37
andresrivera <andresrivera.web@gmail.com>
2023-03-24 21:37:25 -0700

    task 100

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,11 +1,33 @@
-section .data
-	message db "Hello, World",10
-	section .text
 	global main
 
+
+
+	section .text
+
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
+
+	mov     rax, 1
+
+	mov     rdi, 1
+
+	mov     rsi, msg
+
+	mov     rdx, 17
+
 	syscall
+
+
+
+	mov     rax,60
+
+	xor     rdi, rdi
+
+	syscall
+
+
+
+	section .data
+
+msg:
+
+	db   "Hello, Holberton",10
